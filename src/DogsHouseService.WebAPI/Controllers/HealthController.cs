82a725efbb3e58b2fcac089f2f2e3abf81a08bde
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-10-27 10:27:21 +0200

    Implement Ping Endpoint (#11)

---
+++ src/DogsHouseService.WebAPI/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using DogsHouseService.WebAPI.Swagger;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace DogsHouseService.WebAPI.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("ping")]
+        [Produces(MediaTypeNames.Text.Plain)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServerErrorResponseExample), StatusCodes.Status500InternalServerError)]
+        public IActionResult Ping()
+        {
+            return Ok("Dogshouseservice.Version1.0.1");
+        }
+    }
+}
