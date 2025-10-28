e41bb0814fd6a0a4702f0dca98fbb07a72c30ea3
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-10-28 12:45:38 +0200

    Implement Validators Tests

---
+++ tests/DogsHouseService.Application.UnitTests/Validators/CreateDogCommandValidatorTests.cs
@@ -0,0 +1,132 @@
+﻿using DogsHouseService.Application.UseCases.Dogs.Commands.CreateDog;
+using DogsHouseService.Domain.Dogs.DogNames;
+using DogsHouseService.Domain.Dogs.DogWeights;
+using DogsHouseService.Domain.Dogs.TailLengths;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+
+namespace DogsHouseService.Application.UnitTests.Validators
+{
+    public sealed class CreateDogCommandValidatorTests
+    {
+        private readonly CreateDogCommandValidator _validator;
+
+        public CreateDogCommandValidatorTests()
+        {
+            _validator = new CreateDogCommandValidator();
+        }
+
+        [Fact]
+        public void Validate_IfNameIsEmpty_ShouldReturnError()
+        {
+            var command = new CreateDogCommand(
+                Name: "",
+                Color: "Red",
+                TailLength: 10,
+                Weight: 20);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Validate_IfNameLengthExceededMaxLength_ShouldReturnTooLongError()
+        {
+            var longName = new string('a', DogName.MaxLength + 1);
+            var command = new CreateDogCommand(
+                Name: longName,
+                Color: "Red",
+                TailLength: 10,
+                Weight: 20);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Validate_IfColorIsEmpty_ShouldReturnError()
+        {
+            var command = new CreateDogCommand(
+                Name: "Neo",
+                Color: "",
+                TailLength: 10,
+                Weight: 20);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Color);
+        }
+
+        [Fact]
+        public void Validate_IfTailLengthBelowMinimum_ShouldReturnTooSmallError()
+        {
+            var command = new CreateDogCommand(
+                Name: "Neo",
+                Color: "Red",
+                TailLength: TailLength.MinLength - 1,
+                Weight: 20);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.TailLength);
+        }
+
+        [Fact]
+        public void Validate_IfTailLengthAboveMaximum_ShouldReturnTooBigError()
+        {
+            var command = new CreateDogCommand(
+                Name: "Neo",
+                Color: "Red",
+                TailLength: TailLength.MaxLength + 1,
+                Weight: 20);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.TailLength);
+        }
+
+        [Fact]
+        public void Validate_IfWeightBelowMinimum_ShouldReturnTooSmallError()
+        {
+            var command = new CreateDogCommand(
+                Name: "Neo",
+                Color: "Red",
+                TailLength: 10,
+                Weight: DogWeight.MinValue - 1);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Weight);
+        }
+
+        [Fact]
+        public void Validate_IfWeightAboveMaximum_ShouldReturnTooBigError()
+        {
+            var command = new CreateDogCommand(
+                Name: "Neo",
+                Color: "Red",
+                TailLength: 10,
+                Weight: DogWeight.MaxValue + 1);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Weight);
+        }
+
+        [Fact]
+        public void Validate_IfAllFieldsAreValid_ShouldNotReturnError()
+        {
+            var command = new CreateDogCommand(
+                Name: "Neo",
+                Color: "Red",
+                TailLength: 10,
+                Weight: 20);
+
+            var result = _validator.TestValidate(command);
+
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
---
+++ tests/DogsHouseService.Application.UnitTests/Validators/GetDogsQueryValidatorTests.cs
@@ -0,0 +1,131 @@
+﻿using DogsHouseService.Application.UseCases.Dogs.Queries.GetDogs;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+
+namespace DogsHouseService.Application.UnitTests.Validators
+{
+    public class GetDogsQueryValidatorTests
+    {
+        private readonly GetDogsQueryValidator _validator;
+
+        public GetDogsQueryValidatorTests()
+        {
+            _validator = new GetDogsQueryValidator();
+        }
+
+        [Theory]
+        [InlineData("asc")]
+        [InlineData("desc")]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Validate_IfOrderIsValid_ShouldNotReturnError(string order)
+        {
+            var query = new GetDogsQuery(
+                Attribute: "name",
+                Order: order,
+                PageNumber: 1,
+                PageSize: 10);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Order);
+        }
+
+        [Fact]
+        public void Validate_IfOrderIsInvalid_ShouldReturnError()
+        {
+            var query = new GetDogsQuery(
+                Attribute: "name",
+                Order: "invalid",
+                PageNumber: 1,
+                PageSize: 10);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.Order);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Validate_IfPageNumberIsInvalid_ShouldReturnError(int pageNumber)
+        {
+            var query = new GetDogsQuery(
+                Attribute: "name",
+                Order: "asc",
+                PageNumber: pageNumber,
+                PageSize: 10);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.PageNumber);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Validate_IfPageSizeIsInvalid_ShouldReturnError(int pageSize)
+        {
+            var query = new GetDogsQuery(
+                Attribute: "name",
+                Order: "asc",
+                PageNumber: 1,
+                PageSize: pageSize);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.PageSize);
+        }
+
+        [Theory]
+        [InlineData("id")]
+        [InlineData("name")]
+        [InlineData("color")]
+        [InlineData("taillength")]
+        [InlineData("tail-length")]
+        [InlineData("tail_length")]
+        [InlineData("weight")]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Validate_IfAttributeIsValid_ShouldNotReturnError(string attribute)
+        {
+            var query = new GetDogsQuery(
+                Attribute: attribute,
+                Order: "asc",
+                PageNumber: 1,
+                PageSize: 10);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Attribute);
+        }
+
+        [Fact]
+        public void Validate_IfAttributeIsInvalid_ShouldReturnError()
+        {
+            var query = new GetDogsQuery(
+                Attribute: "unknown",
+                Order: "asc",
+                PageNumber: 1,
+                PageSize: 10);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(x => x.Attribute);
+        }
+
+        [Fact]
+        public void Validate_IfAllFieldsAreValid_ShouldNotReturnError()
+        {
+            var query = new GetDogsQuery(
+                Attribute: "name",
+                Order: "asc",
+                PageNumber: 1,
+                PageSize: 10);
+
+            var result = _validator.TestValidate(query);
+
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
